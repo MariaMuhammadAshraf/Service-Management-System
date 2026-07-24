c96ff5022a86c0e0648c32c2a450bdfc82a844c6
MariaMuhammadAshraf <mariaqueen120@gmail.com>
2026-07-24 13:53:27 +0500

    Final fix for service model casing import error

--- backend/server.js
+++ backend/server.js
@@ -34,16 +34,7 @@ app.use("/api/dashboard", dashboardRoutes);
 
 
 
-// const PORT = process.env.PORT || 5000;
-
-// app.get('/', (req, res) => {
-//     res.send('API is running...');
-// });
-
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
-
+ 
 
 // Test Route
 app.get('/', (req, res) => {
