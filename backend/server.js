50d294c83a09806e7a14a891b9bd5ef80be14017
MariaMuhammadAshraf <mariaqueen120@gmail.com>
2026-07-24 16:56:14 +0500

    Fix all route casing paths for vercel

--- backend/server.js
+++ backend/server.js
@@ -10,19 +10,14 @@ import bookingRoutes from './Routes/booking.js';
 import reviewRoutes from './Routes/review.js';
 import dashboardRoutes from './Routes/dashboard.js';
 
-// Load env vars
 dotenv.config();
-
-// Database se connect karein
 connectDB();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/services', serviceRoutes);
 app.use('/api/categories', categoryRoutes);
@@ -30,14 +25,11 @@ app.use('/api/bookings', bookingRoutes);
 app.use('/api/reviews', reviewRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 
-// Test route
 app.get("/", (req, res) => {
     res.status(200).send("service booking Backend is Live and Running!");
 });
 
-// SERVER
 const PORT = process.env.PORT || 5000;
-
 if (process.env.NODE_ENV !== 'production') {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
