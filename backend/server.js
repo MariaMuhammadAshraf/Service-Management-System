6a4cd04aefbe238cc9aeb9ecbdfffa0d2ed03d21
MariaMuhammadAshraf <mariaqueen120@gmail.com>
2026-07-25 14:57:15 +0500

    Force refresh package.json and server for vercel

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
