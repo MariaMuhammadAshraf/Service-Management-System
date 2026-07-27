cc8c7c5eeb68345f8aa518f2b10dc1a40e32e60e
MariaMuhammadAshraf <mariaqueen120@gmail.com>
2026-07-27 14:16:06 +0500

    Allah madad kr

--- frontend/src/Admin/ManageServices.jsx
+++ frontend/src/Admin/ManageServices.jsx
@@ -23,7 +23,7 @@ const ManageServices = () => {
     image: ''
   });
   // --- Yahan API URL set kiya hai ---
-  const API_URL = process.env.REACT_APP_API_URL;
+  // const API_URL = process.env.REACT_APP_API_URL;
 
   // --- API FETCHING ---
   const fetchData = async () => {
--- frontend/src/pages/Service.jsx
+++ frontend/src/pages/Service.jsx
@@ -21,7 +21,7 @@ const Services = () => {
     }
   };
   // --- Yahan API URL set kiya hai ---
-  const API_URL = process.env.REACT_APP_API_URL;
+  // const API_URL = process.env.REACT_APP_API_URL;
 
   useEffect(() => {
    const fetchData = async () => {
