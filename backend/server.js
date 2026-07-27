b51f8be3fc1e5989c5bc7da4e2892bd6271a6482
MariaMuhammadAshraf <mariaqueen120@gmail.com>
2026-07-27 14:27:16 +0500

    Allah madad kr aj meri main haar nahi manana chahti

--- backend/server.js
+++ backend/server.js
@@ -15,13 +15,26 @@ connectDB();
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://service-management-system-orpin.vercel.app");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+    next();
+});
+
 // app.use(cors());
-// Sabse oopar middleware mein yeh likhein:
+// 2. Standard CORS package
 app.use(cors({
-    origin: 'https://service-management-system-orpin.vercel.app',
+    origin: "https://service-management-system-orpin.vercel.app",
     credentials: true
 }));
 
+app.use(express.json());
  
 
 app.use('/api/auth', authRoutes);
